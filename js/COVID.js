8135d8a80efdf8e64e1e917519bc50fb4f4e13ab
Ebbe A <ebbe.arildsson@gmail.com>
2020-04-14 16:35:37 +0200

    color based on deathrate

--- js/COVID.js
+++ js/COVID.js
@@ -1,70 +1,45 @@
-let covidCases
-
-function preload() {
-    let url = 'https://api.covid19api.com/summary'
-    covidCases = loadJSON(url)
-}
+let cc
+preload = () => { cc = loadJSON('https://api.covid19api.com/summary') }
 
 function setup(){
-    createCanvas(0, 0)
+    createCanvas(0, 0)    
+    createTag(cc.Global, "Global")
+    cc.Countries.forEach(country => {
+        createTag(country, country.Country)
+    })
+}
 
+function createTag(country, name){
     var tag = document.createElement("div")
-    var name = document.createElement("div")
-    name.innerHTML = "Global"
-    name.className += "name"
+    var countryName = document.createElement("div")
+    countryName.innerHTML = name
+    countryName.className += "name"
     tag.className += "country"
 
     var dead = document.createElement("div")
-    dead.innerHTML = "Total deaths: " + covidCases.Global.TotalDeaths 
+    dead.innerHTML = "Total deaths: " + country.TotalDeaths 
     dead.className += "info"
 
     var infected = document.createElement("div")
-    infected.innerHTML = "Total confrimed cases: " + covidCases.Global.TotalConfirmed
+    infected.innerHTML = "Total confrimed cases: " + country.TotalConfirmed
     infected.className += "info"
 
     var recovered = document.createElement("div")
-    recovered.innerHTML = "Total recovered: " + covidCases.Global.TotalRecovered
+    recovered.innerHTML = "Total recovered: " + country.TotalRecovered
     recovered.className += "info"
 
     var deathRate = document.createElement("div")
-    deathRate.innerHTML = "Death rate: " + ((covidCases.Global.TotalDeaths / covidCases.Global.TotalConfirmed) * 100).toFixed(2) + "%"
+    deathRate.innerHTML = "Death rate: " + ((country.TotalDeaths / country.TotalConfirmed) * 100).toFixed(2) + "%"
     deathRate.className += "info"
 
-    tag.appendChild(name)
+    tag.appendChild(countryName)
     tag.appendChild(infected)
     tag.appendChild(recovered)
     tag.appendChild(dead)
     tag.appendChild(deathRate)
+    let color = `rgb(${map(country.TotalDeaths / country.TotalConfirmed, 0, 0.15, 100, 255)}, 50, 50)`
+    if(country.TotalConfirmed == 0 || country.TotalDeaths / country.TotalConfirmed == 0) color = '#eeeeee'
+    tag.style.backgroundColor = color;
+    tag.style.color = color;
     document.body.appendChild(tag)
-    
-    covidCases.Countries.forEach(country => {
-        var tag = document.createElement("div")
-        var name = document.createElement("div")
-        name.innerHTML = country.Country
-        name.className += "name"
-        tag.className += "country"
-
-        var dead = document.createElement("div")
-        dead.innerHTML = "Total deaths: " + country.TotalDeaths 
-        dead.className += "info"
-
-        var infected = document.createElement("div")
-        infected.innerHTML = "Total confrimed cases: " + country.TotalConfirmed
-        infected.className += "info"
-
-        var recovered = document.createElement("div")
-        recovered.innerHTML = "Total recovered: " + country.TotalRecovered
-        recovered.className += "info"
-
-        var deathRate = document.createElement("div")
-        deathRate.innerHTML = "Death rate: " + ((country.TotalDeaths / country.TotalConfirmed) * 100).toFixed(2) + "%"
-        deathRate.className += "info"
-
-        tag.appendChild(name)
-        tag.appendChild(infected)
-        tag.appendChild(recovered)
-        tag.appendChild(dead)
-        tag.appendChild(deathRate)
-        document.body.appendChild(tag)
-    })
 }
